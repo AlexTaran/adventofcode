81bf02ddbde44264bb21718555b69a4a61d62301
Alex <alexytaran@gmail.com>
2020-12-18 17:14:44 +0100

    Solved day 9 for 2015

---
+++ 2015/9/solve2.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+from collections import defaultdict
+from itertools import permutations
+
+def main():
+    lines = [l.strip() for l in open('input.txt')]
+    graph = defaultdict(dict)
+    for l in lines:
+      cities, dist = l.split(' = ')
+      f, t = cities.split(' to ')
+      graph[f][t] = int(dist)
+      graph[t][f] = int(dist)
+    res = 0
+    for p in permutations(graph.keys()):
+      tot = 0
+      for i in range(1, len(p)):
+        tot += graph[p[i-1]][p[i]]
+      res = max(res, tot)
+    print(res)
+
+if __name__=="__main__":
+    main()
